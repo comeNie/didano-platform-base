3cd8190b13973ad7d32f710c11a0cf67687f952a
stephenwang2017 <stephen.wang@didano.cn>
2017-04-04 13:06:13 +0800

    增加图片上传模块处理

---
+++ src/main/java/cn/didano/base/interaction/StorageException.java
@@ -0,0 +1,12 @@
+package cn.didano.base.interaction;
+
+public class StorageException extends RuntimeException {
+
+    public StorageException(String message) {
+        super(message);
+    }
+
+    public StorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
---
+++ src/main/java/cn/didano/base/interaction/StorageFileNotFoundException.java
@@ -0,0 +1,12 @@
+package cn.didano.base.interaction;
+
+public class StorageFileNotFoundException extends StorageException {
+
+    public StorageFileNotFoundException(String message) {
+        super(message);
+    }
+
+    public StorageFileNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
